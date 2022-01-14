bec5e055bb1df53471aa4f131e20b856cf58040e
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-14 17:27:14 +0700

    add rollbar in default channels

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['rollbar', 'single'],
             'ignore_exceptions' => false,
         ],
 
