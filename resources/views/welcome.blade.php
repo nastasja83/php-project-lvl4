9c47e9e4c17da15e7f3a2876d304ed1acf802718
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-15 19:48:04 +0700

    create welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="p-5 mb-4 bg-light border rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">{{ __('welcome.Hello from Hexlet!') }}</h1>
+        <p class="col-md-8 fs-4">{{ __('welcome.Practical programming courses!') }}</p>
+        <button href="https://ru.hexlet.io" class="btn btn-primary btn-lg" type="button">{{ __('welcome.Learn more') }}</button>
+    </div>
+</div>
+@endsection('content')
