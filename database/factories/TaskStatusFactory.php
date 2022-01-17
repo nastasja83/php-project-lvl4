a3afe6e3894de1864c75e034d6225d1f488dcbed
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-17 17:29:16 +0700

    create TaskStatusFactory for tests

---
+++ database/factories/TaskStatusFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskStatusFactory extends Factory
+{
+    protected $model = TaskStatus::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
