3dedcf68091f51696d0523908e4aeec1f384d32b
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-19 21:52:27 +0700

    add text translation for tasks

--- resources/lang/ru/tasks.php
+++ resources/lang/ru/tasks.php
@@ -13,5 +13,23 @@ return [
     |
     */
 
-    'Tasks' => 'Задачи'
+    'Tasks' => 'Задачи',
+    'Create' => 'Создать',
+    'Create task' => 'Создать задачу',
+    'Edit' => 'Изменить',
+    'Delete' => 'Удалить',
+    'Update' => 'Обновить',
+    'Date of creation' => 'Дата создания',
+    'Actions' => 'Действия',
+    'Apply' => 'Применить',
+    'Task name' => 'Имя',
+    'Author' => 'Автор',
+    'Description' => 'Описание',
+    'Executor' => 'Исполнитель',
+    'Edit task' => 'Изменение задачи',
+    'View a task' => 'Просмотр задачи',
+    'Task has been added successfully' => 'Задача успешно создана',
+    'Task has been updated successfully' => 'Задача успешно изменена',
+    'Task has been deleted successfully' => 'Задача успешно удалена',
+    'Are you sure?' => 'Вы уверены?'
 ];
