b3c61e850a8f8c09ff84ed6bf892ed008844b1a7
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-20 22:15:12 +0700

    create LabelControllerTest

---
+++ tests/Feature/LabelControllerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Label;
+
+class LabelControllerTest extends TestCase
+{
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('labels.index'));
+        $response->assertOk();
+    }
+
+    public function testCreate()
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('labels.create'));
+        $response->assertOk();
+    }
+
+    public function testEdit()
+    {
+        $label = Label::factory()->create();
+
+        $response = $this->actingAs($this->user)
+            ->get(route('labels.edit', ['label' => $label]));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = Label::factory()
+        ->make()
+        ->only(['name', 'description']);
+
+        $response = $this->actingAs($this->user)
+            ->post(route('labels.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('labels.index'));
+        $this->get(route('labels.index'))->assertSee($data['name']);
+        $this->assertDatabaseHas('labels', $data);
+    }
+
+    public function testUpdate()
+    {
+        $label = Label::factory()->create();
+
+        $data = Label::factory()
+        ->make()
+        ->only(['name']);
+
+        $response = $this->actingAs($this->user)
+            ->patch(route('labels.update', ['label' => $label]), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('labels.index'));
+        $this->get(route('labels.index'))->assertSee($data['name']);
+        $this->assertDatabaseHas('labels', $data);
+    }
+
+    public function testDestroy()
+    {
+        $label = Label::factory()->create();
+        $response = $this->actingAs($this->user)
+            ->delete(route('label.destroy', ['label' => $label]));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('label.index'));
+
+        $this->assertDatabaseMissing('label', ['id' => $label->id]);
+    }
+}
