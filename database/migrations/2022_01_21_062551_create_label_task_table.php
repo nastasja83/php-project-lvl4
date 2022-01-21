a1ce1c2e582c7a0921f1d352deacc771175ea812
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-21 16:01:56 +0700

    create migration create_label_task_table

---
+++ database/migrations/2022_01_21_062551_create_label_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_task', function (Blueprint $table) {
+            $table->foreignId('task_id')->constrained('tasks');
+            $table->foreignId('label_id')->constrained('labels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('label_task');
+    }
+}
