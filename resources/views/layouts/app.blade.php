52e8620b64cba5dee31007dd0ad355552d4df292
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-21 21:43:07 +0700

    change background color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-white">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
