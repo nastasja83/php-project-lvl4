bba8c4b8e802050244f56fe541739749715428c0
Anastasiya Ostertag <nastasja83@mail.ru>
2022-01-22 19:18:02 +0700

    fix errors block

--- resources/views/task_statuses/create.blade.php
+++ resources/views/task_statuses/create.blade.php
@@ -6,13 +6,13 @@
     <div class="form-group mb-3">
         {{Form::label('name', __('taskStatuses.Status name'))}}
         {{Form::text('name', '', ['class' => 'form-control'])}}
-    </div>
-    <div class="invalid-feedback d-block">
+        <div class="invalid-feedback d-block">
         @if ($errors->any())
             @foreach ($errors->all() as $error)
                 {{ $error }}
             @endforeach
         @endif
+        </div>
     </div>
         {{Form::submit(__('taskStatuses.Create'), ['class' => 'btn btn-primary mt-3'])}}
 {{ Form::close() }}
